f59e143dff9686f86d7ceb282aa054e180934b4f
root <root@FRUH124476.localdomain>
2025-12-01 23:46:35 +0100

    Definition de la classe Game.h

--- src/Game.h
+++ src/Game.h
@@ -6,3 +6,15 @@
 #include "Renderer.h"
 #include <memory>
 
+class Game {
+private:
+    Grid grid_;
+    std::unique_ptr<RuleSet> rules_;
+    std::unique_ptr<Renderer> renderer_;
+public:
+    Game(Grid initial, std::unique_ptr<RuleSet> rules, std::unique_ptr<Renderer> renderer);
+    void runConsole(int maxIterations);
+    bool isStable() const;
+};
+
+#endif // GAME_H
