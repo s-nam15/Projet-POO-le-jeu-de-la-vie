280a0440a4352bf4816595a2dd166abdeb5fed5e
root <root@FRUH124476.localdomain>
2025-12-02 14:10:40 +0100

    Creation du dossier Larry_Brown

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,6 +3,7 @@
 #include "ConsoleRenderer.h"
 #include <iostream>
 #include <filesystem>
+namespace fs = std::filesystem;
 
 Game::Game(Grid initial, std::unique_ptr<RuleSet> rules, std::unique_ptr<Renderer> renderer)
     : grid_(std::move(initial)), rules_(std::move(rules)), renderer_(std::move(renderer)) {}
@@ -14,25 +15,29 @@ bool Game::isStable() const {
 
 void Game::runConsole(int maxIterations) {
     Grid current = grid_;
+
+    std::string outFolder = "output_console";
     
-    std::string base = "output/";
-    std::filesystem::create_directory(base);
+    //  Supprime le dossier s'il existe déjà
+    if (fs::exists(outFolder)) {
+        fs::remove_all(outFolder);
+    }
+    //  Le recrée proprement
+    fs::create_directory(outFolder);
 
     for (int iter = 0; iter < maxIterations; ++iter) {
         std::cout << "Iteration " << iter << ":\n";
         renderer_->render(current);
 
-        // Génération du fichier de sortie
-        std::string filename = base + "iteration_" + std::to_string(iter) + ".txt";
-        renderer_->saveToFile(current, filename);
+        // Sauvegarde dans un fichier
+        std::string filename = outFolder + "/iteration_" + std::to_string(iter) + ".txt";
+        current.toFile(filename);
 
         Grid next = current.computeNextGrid(*rules_);
-
         if (next == current) {
             std::cout << "Stable après " << iter << " itérations.\n";
             break;
         }
-
-        current = next;
+        current = std::move(next);
     }
 }
